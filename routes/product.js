3cf871bdd901d4cec593e326da6ff1043e2f96c8
Cédric <cedric.olivry@sfr.fr>
2020-11-27 19:47:46 +0100

    modif

--- routes/product.js
+++ routes/product.js
@@ -7,21 +7,21 @@ const multer = require('../middleware/multer-config');
 const productCtrl = require('../controllers/product');
 
 /* GET ALL */
-router.get('/', productCtrl.getAllSauces);
+router.get('/', auth, productCtrl.getAllSauces);
 
 /* GET ONE BY ID */
-router.get('/:id', productCtrl.getOneSauce);
+router.get('/:id', auth, productCtrl.getOneSauce);
 
 /* POST ONE / ADD ONE */
 router.post('/', auth, multer, productCtrl.createSauce);
 
 /* UPDATE ONE */
-router.put('/:id', multer, productCtrl.modifySauce);
+router.put('/:id', auth, multer, productCtrl.modifySauce);
 
 /* DELETE ONE */
-router.delete('/:id', productCtrl.deleteSauce);
+router.delete('/:id', auth, productCtrl.deleteSauce);
 
 /*LIKE DISLIKE*/
-router.post('/:id/like', productCtrl.likeSauce);
+router.post('/:id/like', auth, productCtrl.likeSauce);
 
 module.exports = router;
